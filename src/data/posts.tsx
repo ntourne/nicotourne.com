27b0b45ff5d86fb2d2935d872923acf112c16bd1
Nicolas Tourne <nicotourne@gmail.com>
2023-08-24 09:18:44 -0300

    feat: Add new article

--- src/data/posts.tsx
+++ src/data/posts.tsx
@@ -1,5 +1,11 @@
 export const POSTS = [
   {
+    title:
+      "Living on the Edge: How Computing’s New Frontier Is Closer Than You Think!",
+    url: "https://medium.com/beereal/living-on-the-edge-how-computings-new-frontier-is-closer-than-you-think-a3f1d72f1a80",
+    createdDate: "Aug 24, 2023",
+  },
+  {
     title: "How will AI change the way we Write Software?",
     url: "https://medium.com/beereal/how-will-ai-change-the-way-we-write-software-b52601cbffc1",
     createdDate: "Mar 6, 2023",
