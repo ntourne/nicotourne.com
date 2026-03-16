74a979ae810872661af498829a6004f4c375c323
Nicolas Tourne <nicotourne@gmail.com>
2026-03-16 10:25:35 -0300

    chore: Clean

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/lote-aqua-mdp',
-        destination: 'https://docs.google.com/document/d/1aDnKb1Cfmt9X80xNuP3xvP81qNv3urrphG1eEJCywMM/edit?tab=t.0',
-        permanent: false,
-      },
-    ]
-  },
 }
